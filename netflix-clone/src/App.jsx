67b0d4168606e624f75bd730f1ef15935ac7bb80
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-09-02 12:03:31 -0400

    update

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login'
+import Four04 from "./pages/NotFound404/NotFound404";
+
+
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Four04 />} />
       </Routes>
     </div>
   );
