63dd5193b82a06b0861904309498cbba3f554729
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-09-02 08:01:06 -0400

    update

--- netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,46 @@
-import './TitleCards.css'
+import { useRef, useEffect } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = () => {
-  return (
-    <div className='titlecards'>
-      
-    </div>
-  )
-}
+  
 
-export default TitleCards
+   const cardsRef = useRef(null);
+
+   const handleWheel = (event) => {
+     event.preventDefault();
+     cardsRef.current.scrollLeft += event.deltaY; // ✅ fixed typo
+   };
+
+   useEffect(() => {
+     const container = cardsRef.current;
+     if (container) {
+       container.addEventListener("wheel", handleWheel, { passive: false });
+     }
+
+     // ✅ cleanup
+     return () => {
+       if (container) {
+         container.removeEventListener("wheel", handleWheel);
+       }
+     };
+   }, []);
+
+  return (
+    <div className="titlecards">
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt={card.name} />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from "../../assets/hero_title.png";
 import play_icon from "../../assets/play_icon.png";
 import info_icon from "../../assets/info_icon.png";
+import TitleCards from '../../components/TitleCards/TitleCards';
 
 
 
@@ -30,7 +31,8 @@ const Home = () => {
               <img src={info_icon} alt="info_icon" />
               More Info
             </button>
-          </div>
+                  </div>
+                  <TitleCards />
         </div>
       </div>
     </div>
