143f10a2c46d6cefd6140066606c5b6503eeb489
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-09-03 04:50:52 -0400

    update

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1Jy9gGkoUoSHvi9D51Rx75_4Fu7MjhPg",
+  authDomain: "netflix-clone-70025.firebaseapp.com",
+  projectId: "netflix-clone-70025",
+  storageBucket: "netflix-clone-70025.firebasestorage.app",
+  messagingSenderId: "1030002504527",
+  appId: "1:1030002504527:web:e83e2c3e1b4baa09738b3a"
+};
+
+const app = initializeApp(firebaseConfig);
