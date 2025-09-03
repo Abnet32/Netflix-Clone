de3273c841daa03f2d7b279a522184180c2d1841
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-09-03 03:45:38 -0400

    update

--- netflix-clone/src/components/Navbar/Navbar.jsx
+++ netflix-clone/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,29 @@
-import './Navbar.css'
-import logo from '../../assets/logo.png'
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search_icon.svg";
 import bell_icon from "../../assets/bell_icon.svg";
 import profile_img from "../../assets/profile_img.png";
 import caret_icon from "../../assets/caret_icon.svg";
+import { useEffect, useRef } from "react";
 
 const Navbar = () => {
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 80) {
+        navRef.current?.classList.add("nav-dark");
+      } else {
+        navRef.current?.classList.remove("nav-dark");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="navbar">
+    <div ref={navRef} className="navbar">
       <div className="navbar-left">
         <img src={logo} alt="logo" />
         <ul>
@@ -25,14 +41,14 @@ const Navbar = () => {
         <img src={bell_icon} alt="bell_icon" className="icons" />
         <div className="navbar-profile">
           <img src={profile_img} alt="profile_img" className="profile" />
-                  <img src={caret_icon} alt="caret_icon" />
-                  <div className="dropdown">
-                      <p>Sign Out of Netflix</p>
-                  </div>
+          <img src={caret_icon} alt="caret_icon" />
+          <div className="dropdown">
+            <p>Sign Out of Netflix</p>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
