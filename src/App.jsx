89d5193186a710027697fdaddc0bc56d3b6e0b74
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-10-27 04:50:01 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Four04 from "./pages/NotFound404/NotFound404";
 import Player from "./pages/Player/Player";
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { auth } from "./firebase";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { useEffect } from "react";
+// import { auth } from "./firebase";
 import { ToastContainer } from "react-toastify";
 
 const App = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        console.log("Logged In");
-        navigate("/");
-      } else {
-        console.log("Logged Out");
-        navigate("/login");
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   onAuthStateChanged(auth, async (user) => {
+  //     if (user) {
+  //       console.log("Logged In");
+  //       navigate("/");
+  //     } else {
+  //       console.log("Logged Out");
+  //       navigate("/login");
+  //     }
+  //   });
+  // }, []);
 
   return (
     <div>
       <ToastContainer theme="dark" />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/player/:id" element={<Player />} />
         <Route path="*" element={<Four04 />} />
       </Routes>
